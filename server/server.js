909d0dc27fe9506dcedc11147dd38f2234be7e00
MOHAN VIGNESH <kmvk777@gmail.com>
2025-06-23 01:18:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://teamahall.netlify.app/",
+    origin: "https://teamahall.netlify.app",
     credentials: true,
   })
 );
