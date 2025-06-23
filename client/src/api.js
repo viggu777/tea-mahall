6c92cf2734869dfa063765c063d40b8b351216ce
MOHAN VIGNESH <kmvk777@gmail.com>
2025-06-23 11:05:13 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://tea-mahall.onrender.com";
+
 const API = axios.create({
- baseURL: "https://tea-mahall.onrender.com",
+  baseURL,
 });
 
 export default API;
